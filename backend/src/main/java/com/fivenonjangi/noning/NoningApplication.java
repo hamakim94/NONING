05d004ccd1829ac14fbc7e3bd0624f990617bdda
이재순 <dlwotns1112@gmail.com>
2022-07-23 01:53:37 +0900

    BE_220723_Spring Security 임시 중단: NoningApplication.java 수정 완료

--- backend/src/main/java/com/fivenonjangi/noning/NoningApplication.java
+++ backend/src/main/java/com/fivenonjangi/noning/NoningApplication.java
@@ -2,8 +2,9 @@ package com.fivenonjangi.noning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class NoningApplication {
 
     public static void main(String[] args) {
