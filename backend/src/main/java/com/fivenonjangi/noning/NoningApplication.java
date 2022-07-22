857d413fa7af635074d43e85bca6eeeac6ca2f09
이재순 <dlwotns1112@gmail.com>
2022-07-22 23:41:43 +0900

    BE_220722_create project: 스프링 부트 프로젝트 생성

---
+++ backend/src/main/java/com/fivenonjangi/noning/NoningApplication.java
@@ -0,0 +1,13 @@
+package com.fivenonjangi.noning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoningApplication.class, args);
+    }
+
+}
