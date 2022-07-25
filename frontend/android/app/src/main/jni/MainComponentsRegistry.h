64d98d4e49bd5d5b5c625d3f778f9ec630815924
이도엽 <lx460@naver.com>
2022-07-25 11:45:33 +0900

    FE_220725_frontend:create frontend

--- frontend/android/app/src/main/jni/MainComponentsRegistry.h
+++ frontend/android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/doyeop/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/frontend/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
