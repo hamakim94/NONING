748127dbb5f7e559960c78527fc7c8df03a0d692
이승연 <seongyeon38@gmail.com>
2022-07-27 15:16:55 +0900

    BE_220727_writeComment: comment 작성 api 수정 완료

---
+++ backend/src/main/java/com/fivenonjangi/noning/data/entity/comment/CommentData.java
@@ -0,0 +1,36 @@
+package com.fivenonjangi.noning.data.entity.comment;
+
+import com.fivenonjangi.noning.data.dto.comment.CommentDataDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "comment_data")
+public class CommentData {
+    @Id
+    @Column(name = "comment_data_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    int likes;
+    int dislikes;
+    @OneToOne
+    @JoinColumn(name = "comment_id")
+    Comment comment;
+
+    public CommentDataDTO toDTO(){
+        return CommentDataDTO.builder()
+                .id(id)
+                .likes(likes)
+                .dislikes(dislikes)
+                .comment(comment.toDto())
+                .build();
+    }
+}
