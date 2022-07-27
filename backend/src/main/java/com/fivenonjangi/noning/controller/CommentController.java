13d65b0570240f31e76b859f2ad9055ad074d257
이승연 <seongyeon38@gmail.com>
2022-07-27 11:02:46 +0900

    BE_220727_writeComment: comment 작성 api 개발 완료

---
+++ backend/src/main/java/com/fivenonjangi/noning/controller/CommentController.java
@@ -0,0 +1,25 @@
+package com.fivenonjangi.noning.controller;
+
+import com.fivenonjangi.noning.data.dto.comment.CommentRequestDTO;
+import com.fivenonjangi.noning.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/boards/{boardid}/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping("/write")
+    public ResponseEntity writeComment(@PathVariable("boardid") long boardId, @RequestBody CommentRequestDTO commentRequestDTO){
+        commentService.writeComment(boardId, commentRequestDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ backend/src/main/java/com/fivenonjangi/noning/data/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.fivenonjangi.noning.data.repository;
+
+import com.fivenonjangi.noning.data.entity.board.Board;
+import com.fivenonjangi.noning.data.entity.comment.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
