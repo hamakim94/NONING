effe07e5a7b2bf542c3e41e5c8ee0d279cc565bd
이도엽 <lx460@naver.com>
2022-07-27 17:03:31 +0900

    FE_220727_plus & nav: fix error 수정완료

--- frontend/src/screens/board/DetailScreen.js
+++ frontend/src/screens/board/DetailScreen.js
@@ -3,10 +3,26 @@ import React from 'react';
 
 export default function DetailScreen() {
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View style={styles.container}>
+      <View style={{flex: 6, borderWidth: 2}}>
+        <Text>DetailScreen1</Text>
+      </View>
+      <View style={{flex: 0, borderWidth: 2}}>
+        <Text>DetailScreen2</Text>
+      </View>
+      <View style={{flex: 6, borderWidth: 2}}>
+        <Text>DetailScreen3</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: '1%',
+    paddingHorizontal: '5%',
+    backgroundColor: 'white',
+  },
+});
