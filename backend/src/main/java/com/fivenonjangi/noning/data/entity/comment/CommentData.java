594b9f52721733c6e5da20aa3e6af81df23e2494
이승연 <seongyeon38@gmail.com>
2022-07-29 11:25:32 +0900

    BE_220729_boardLike: 게시글 좋아요,싫어요 api 수정(code 0,1,2) 완료

--- backend/src/main/java/com/fivenonjangi/noning/data/entity/comment/CommentData.java
+++ backend/src/main/java/com/fivenonjangi/noning/data/entity/comment/CommentData.java
@@ -25,20 +25,30 @@ public class CommentData {
     @JoinColumn(name = "comment_id")
     Comment comment;
 
-    public void like(boolean isExist){
-        if(isExist){
+    public void like(int code){
+//        0: X(중립) → 좋아요
+//        1: 싫어요 → 좋아요
+//        2: 좋아요 → 좋아요 취소
+        if(code == 0){
+            this.likes++;
+        } else if(code == 1) {
             this.dislikes--;
             this.likes++;
-        } else {
-            this.likes++;
+        } else if(code == 2) {
+            this.likes--;
         }
     }
 
-    public void dislike(boolean isExist){
-        if(isExist){
-            this.likes--;
+    public void dislike(int code){
+//        0: X(중립) → 싫어요
+//        1: 싫어요 → 싫어요 취소
+//        2: 좋아요 → 싫어요
+        if(code == 0){
             this.dislikes++;
-        } else {
+        } else if(code == 1) {
+            this.dislikes--;
+        } else if(code == 2) {
+            this.likes--;
             this.dislikes++;
         }
     }
