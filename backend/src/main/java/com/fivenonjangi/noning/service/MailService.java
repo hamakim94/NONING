b9b4cfec79dd069f7a653d368969bb2bfa1d33d3
이재순 <dlwotns1112@gmail.com>
2022-08-01 23:19:18 +0900

    BE_220801_smtp:smtp 추가 진행중

--- backend/src/main/java/com/fivenonjangi/noning/service/MailService.java
+++ backend/src/main/java/com/fivenonjangi/noning/service/MailService.java
@@ -1,4 +1,12 @@
 package com.fivenonjangi.noning.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 }
