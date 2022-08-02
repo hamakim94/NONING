c3db209be3d95efa4bd2a1ea70f592606dbdb5bf
이재순 <dlwotns1112@gmail.com>
2022-08-02 12:00:58 +0900

    BE_220802_smtp_findpassword:비밀번호 찾기 임시 비밀번호 발급 완료

--- backend/src/main/java/com/fivenonjangi/noning/service/MailService.java
+++ backend/src/main/java/com/fivenonjangi/noning/service/MailService.java
@@ -2,4 +2,6 @@ package com.fivenonjangi.noning.service;
 
 public interface MailService {
     void sendVerifyMail(String email, String token) throws Exception;
+
+    void sendPasswordMail(String email, String password) throws Exception;
 }
