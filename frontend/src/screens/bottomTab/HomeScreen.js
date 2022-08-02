a70e37e68f79fa0cb3b1b1a727884c65f9ce3f21
김민균 <mingyun0324@gmail.com>
2022-08-03 02:16:30 +0900

    FE_220803_home-filter:보드 수정과 렌더링 순서가 안 ㅁ자음

--- frontend/src/screens/bottomTab/HomeScreen.js
+++ frontend/src/screens/bottomTab/HomeScreen.js
@@ -5,6 +5,7 @@ import LogoSearch from '../../components/home/LogoSearch';
 import FilterButtonTabs from '../../components/home/FilterButtonTabs';
 import Boards from '../../components/home/Boards';
 import axios from 'axios';
+import { BOARDS } from '../../data/boards';
 
 // 게시글 가져오기 :  /api/boards/list/{userid}  인풋 : userId, categoryCode, order?categorycode=””
 function HomeScreen({navigation}) {
@@ -13,18 +14,60 @@ function HomeScreen({navigation}) {
   const [temp_boards, setTempBoards] = useState([]);
   const [start_num, setStartNum] = useState(0);
   const [loading, setLoading] = useState(false);
+
+  const filterToCode = {
+    '전체' : 0,
+    '연애' : 'B0101',
+    '병맛' : 'B0102',
+    '음식' : 'B0103',
+    '게임' : 'B0104',
+    '운동' : 'B0105',
+    '학교' : 'B0106',
+    '직장' : 'B0107',
+    '갈등' : 'B0108',
+    '기타' : 'B0199',
+  } 
   useEffect(() => {
-    async function get() {
-      const {data} = await axios.get( // 처음 실행하는 함수, 전체 보드를 가져온다
-        'http://i7a202.p.ssafy.io:9999/api/boards/list',
-        {
-          params: {categorycode: 0},
-        },
-      ); //이렇게 요청하면, 매번 url에 새로운 객체 주소를 받아오기 떄문에, 계속 무한
-      setBoards(data);
+    async function get(categoryCode) {
+      // const {data} = await axios.get( // 처음 실행하는 함수, 전체 보드를 가져온다
+      //   'http://i7a202.p.ssafy.io:9999/api/boards/list',
+      //   {
+      //     params: {categorycode: filterToCode[filterName]},
+      //   },
+      // ); //이렇게 요청하면, 매번 url에 새로운 객체 주소를 받아오기 떄문에, 계속 무한
+      // setBoards(data);
+      // console.log(data)
+      //----------------------------------삭제-----------------------
+      const temp = [...BOARDS].filter(board => board.categoryCode === categoryCode)
+      console.log(temp);
+      setBoards(temp)
+      console.log(categoryCode);
+      //----------------------------------삭제-----------------------
     }
-    get();
-  }, []);
+    get(filterToCode[filterName]);
+    // console.log(1 + ' useEffect')
+    const getData = start => {
+      if (boards.length > start + 10) {
+        setLoading(true);
+        setTempBoards(temp_boards.concat([...boards].slice(start, start + 10)));
+        setStartNum(start_num + 10);
+        setLoading(false);
+      } else if (boards.length > start) {
+        setLoading(true);
+        setTempBoards(temp_boards.concat([...boards]));
+        setStartNum(start_num + boards.length);
+        setLoading(false);
+      }
+    };
+    
+    // 처음 렌더링 될 떄 , 10개 이하만 가져오기
+    if (temp_boards.length === 0) {
+      getData(0);
+    }
+    
+  }, [filterName]);
+
+  // 여기서 board가 바뀌면 temp_board 복사를 딱 해야해!
 
   // 전체 가져온 데이터를 10개보다 작으면 그 개수만큼, 아니면 10개씩 복사
   const getData = start => {
@@ -40,10 +83,11 @@ function HomeScreen({navigation}) {
       setLoading(false);
     }
   };
-  // 처음 렌더링 될 떄 , 10개 이하만 가져오기
-  if (temp_boards.length === 0) {
-    getData(0);
-  }
+  
+  // // 처음 렌더링 될 떄 , 10개 이하만 가져오기
+  // if (temp_boards.length === 0) {
+  //   getData(0);
+  // }
  
   const onEndReached = () => { // 끝을 만나면. 
     if (loading) {             // 로딩 중이면 그만하고, 로딩중이 아니라면 getData 하렴!
@@ -56,12 +100,10 @@ function HomeScreen({navigation}) {
   const renderItem = ({item}) => (
     <Boards
       board={item}
-      boards={temp_boards}
-      setBoards={setTempBoards}
       navigation={navigation}></Boards>
   );
-  const keyExtractor = item => item.boardId;
 
+  const keyExtractor = item => item.boardId;
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{flex: 1, padding: 16}}>
