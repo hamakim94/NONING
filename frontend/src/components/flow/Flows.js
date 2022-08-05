2720ca8d7b9910530d327fadbb1fbd90beda7d60
이도엽 <lx460@naver.com>
2022-08-05 15:40:43 +0900

    FE_220805_flow:화면렌더링수정

--- frontend/src/components/flow/Flows.js
+++ frontend/src/components/flow/Flows.js
@@ -1,26 +1,28 @@
-import {StyleSheet, Text, View, ActivityIndicator, Dimensions} from 'react-native';
+import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import React, {useState} from 'react';
-import FlowBar from './FlowBar'
+import FlowBar from './FlowBar';
 import FlowBottom from './FlowBottom';
 import FireAnimation from '../animations/Fire';
 import WaveAnimation from '../animations/Wave';
 
 const windowHeight = Dimensions.get('window').height;
 
-export default function Flows({board, boards, setBoards, navigation}) {
-  console.log('보드렌더링');
+function Flows({board, navigation}) {
   const [boardData, setBoardData] = useState(board);
-
+  console.log('1번');
   return (
-    <View style={{height:windowHeight*0.9}}>
-      <WaveAnimation/>
-      <FireAnimation/>
+    <View style={{height: windowHeight * 0.935}}>
+      <WaveAnimation />
+      <FireAnimation />
       <View style={styles.container}>
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>{board.title}</Text>
         </View>
         <FlowBar board={boardData} setBoards={setBoardData}></FlowBar>
-        <FlowBottom  board={boardData} setBoards={setBoardData} navigation={navigation}></FlowBottom>
+        <FlowBottom
+          board={boardData}
+          setBoards={setBoardData}
+          navigation={navigation}></FlowBottom>
       </View>
     </View>
   );
@@ -28,9 +30,9 @@ export default function Flows({board, boards, setBoards, navigation}) {
 
 const styles = StyleSheet.create({
   container: {
-    borderWidth: 3,
     alignSelf: 'center',
     width: '100%',
+    height: '100%',
     justifyContent: 'center',
     flex: 1,
   },
@@ -47,3 +49,5 @@ const styles = StyleSheet.create({
     flex: 0.35,
   },
 });
+
+export default React.memo(Flows);
--- frontend/src/screens/bottomTab/FlowScreen.js
+++ frontend/src/screens/bottomTab/FlowScreen.js
@@ -1,18 +1,11 @@
-import React, {useCallback, useContext, useEffect, useMemo, useState} from 'react';
-import { TouchableOpacity, StyleSheet, View, SafeAreaView, FlatList, Dimensions } from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {StyleSheet, View, FlatList} from 'react-native';
 import Flows from '../../components/flow/Flows';
-import {BOARDS} from '../../data/boards';
 import UseAxios from '../../util/UseAxios';
-import UserContext from '../../util/UserContext';
 
 // 게시글 가져오기 :  /api/boards/list/{userid}  인풋 : userId, categoryCode, order?categorycode=””
 function FlowScreen({navigation}) {
-
-  const [filterName, setFilterName] = useState('전체');
   const [boards, setBoards] = useState([]);
-  const [temp_boards, setTempBoards] = useState([])
-  const [start_num, setStartNum] = useState(0);
-  const [loading, setLoading] = useState(false);
   // 처음 실행하는 함수, 전체 보드 를 가져오는데, 이를 먼저 수정해
   // useEffect(() => {
   //   (async () => {
@@ -21,80 +14,33 @@ function FlowScreen({navigation}) {
   //   })();
   // }, []);
 
-  const filterToCode = {
-    전체: 0,
-    연애: 'B0101',
-    병맛: 'B0102',
-    음식: 'B0103',
-    게임: 'B0104',
-    운동: 'B0105',
-    학교: 'B0106',
-    직장: 'B0107',
-    갈등: 'B0108',
-    기타: 'B0199',
-  };
-  useEffect(() => {
-    UseAxios.get('/boards/list', {
-      params: {categorycode: filterToCode[filterName]},
-    }).then(res => {
-      setBoards(res.data)
-    })
-  }, [filterName]);
-
   useEffect(() => {
     UseAxios.get('/boards/flow').then(res => {
-      setBoards(res.data)
-    })
+      setBoards(res.data);
+      console.log(res.data);
+    });
   }, []);
 
-
-  // 전체 가져온 데이터를 복사하는 함수.
-  const getData = async (start) => {
-    if(start + 10 < boards.length ){
-      console.log('복사 시작..')
-      setLoading(true)
-      setTempBoards(temp_boards.concat([...boards].slice(start, start+10)));
-      setStartNum(start_num+10) ;
-      setLoading(false)
-    }
-  };
-  // 처음 렌더링 될 떄 , 10개만 가져오기s
-  if(temp_boards.length === 0){
-    getData(0)
-  }
-  // 끝을 만나면.
-  const onEndReached = () => {
-    // 로딩 중이면 그만하고, 로딩중이 아니라면 getData 하렴!
-    if (loading) {
-      return;
-    } else {
-      getData(start_num);
-    }
-  }
   const renderItem = useCallback(
-    ({item}) => <Flows board={item} boards={temp_boards} setBoards={setTempBoards} navigation={navigation}></Flows>,
-    []
+    ({item}) => <Flows board={item} navigation={navigation}></Flows>,
+    [],
   );
-  const keyExtractor = useCallback((item) => item.board_id, []);
+  const keyExtractor = useCallback(item => item.boardId, []);
 
   return (
-      <View style={{flex: 1, margin: 0}}>
-        <FlatList
-          showsVerticalScrollIndicator={false}
-          removeClippedSubviews={true}
-          legacyImplementation={true}
-          data={temp_boards}
-          renderItem={renderItem}
-          keyExtractor={keyExtractor}
-          onEndReached = {onEndReached}
-          onEndReachedThreshold={1}
-          disableVirtualization={false} 
-          ListFooterComponent={loading && <ActivityIndicator size="large" color="#00ff00"/>}
-          >
-        </FlatList>
-      </View>
+    <View style={{flex: 1}}>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        removeClippedSubviews={true}
+        legacyImplementation={true}
+        initialNumToRender={3}
+        windowSize={3}
+        data={boards}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        disableVirtualization={false}></FlatList>
+    </View>
   );
 }
-const styles = StyleSheet.create({
-});
+const styles = StyleSheet.create({});
 export default FlowScreen;
