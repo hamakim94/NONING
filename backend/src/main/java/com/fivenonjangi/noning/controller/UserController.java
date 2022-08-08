f90c56996c0c0a4be378e96145ba6ae41c909b3d
이재순 <dlwotns1112@gmail.com>
2022-08-08 16:53:07 +0900

    BE_220808_fileUpload Hotfix:업로드 수정 완료

--- backend/src/main/java/com/fivenonjangi/noning/controller/UserController.java
+++ backend/src/main/java/com/fivenonjangi/noning/controller/UserController.java
@@ -9,12 +9,8 @@ import com.fivenonjangi.noning.service.board.BoardService;
 import com.fivenonjangi.noning.service.etc.AwsS3Service;
 import com.fivenonjangi.noning.service.user.FollowService;
 import com.fivenonjangi.noning.service.user.UserService;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Encoding;
-import io.swagger.v3.oas.annotations.media.Schema;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -44,7 +40,9 @@ public class UserController {
     private final AwsS3Service awsS3Service;
 
     @PostMapping("/signup")
-    public ResponseEntity signupUser(@RequestPart(value = "signupRequestDTO") SignupRequestDTO signupRequestDTO, @RequestPart(value = "file", required = false) MultipartFile image) {
+    public ResponseEntity signupUser(@RequestPart(value = "signupRequestDTO") String signupRequestDTOString, @RequestPart(value = "image", required = false) MultipartFile image) {
+        Gson gson = new Gson();
+        SignupRequestDTO signupRequestDTO = gson.fromJson(signupRequestDTOString, SignupRequestDTO.class);
         if (image != null&&image.getContentType().startsWith("image")){
             try {
                 String img = awsS3Service.uploadFileV1("profileImg", image);
@@ -110,7 +108,9 @@ public class UserController {
         }
     }
     @PutMapping("/profiles/edit")
-    public ResponseEntity modifyUser(@RequestPart(value = "userDTO") UserDTO userDTO, @RequestPart(value = "file", required = false) MultipartFile image, HttpServletRequest request){
+    public ResponseEntity modifyUser(@RequestPart(value = "userDTO") String userDTOString, @RequestPart(value = "image", required = false) MultipartFile image, HttpServletRequest request){
+        Gson gson = new Gson();
+        UserDTO userDTO = gson.fromJson(userDTOString, UserDTO.class);
         if (jwtTokenProvider.getUserPk(jwtTokenProvider.resolveToken(request, "ACCESSTOKEN")).equals(String.valueOf(userDTO.getUserId()))) {
             try {
                 if (image != null&&image.getContentType().startsWith("image")){
