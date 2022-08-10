0c4c583fdb1a9981253ad250ec4b94dcc90cb7c2
이재순 <dlwotns1112@gmail.com>
2022-08-10 21:34:26 +0900

    FE,BE_220810_Hotfix:로컬주소 수정 완료

--- chatserver/src/UseAxios.js
+++ chatserver/src/UseAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const useAxios = axios.create({
-  baseURL: 'http://localhost:8888/api',
+  baseURL: 'http://i7a202.p.ssafy.io:8888/api',
 });
 
 export default useAxios;
