3dc35fee35e6d2f21bef8b0316051822f124b08b
이재순 <dlwotns1112@gmail.com>
2022-08-10 22:01:03 +0900

    BE_220810_CleanUp:clenUp 완료

--- chatserver/src/server.js
+++ chatserver/src/server.js
@@ -23,39 +23,39 @@ io.on('connection', (socket) => {
 
   // 실시간 음성채팅방 입장
   socket.on('enter', (boardData, userData, done) => {
-    socket.join(boardData.boardId); // 방 들어감
-
-    const userVoteData = {
-      userId: userData.userId,
-      nickname: userData.nickname,
-      img: userData.img,
-      userVote: boardData.userVote,
-    };
-
-    socket.userVoteData = userVoteData;
-    socket.boardId = boardData.boardId;
-
-    if (userList.get(boardData.boardId) == undefined)
-      userList.set(boardData.boardId, new Set());
-
-    userList.get(boardData.boardId).add(socket); // back에서 가지고 있을 userList (나중에 새로 들어온 사용자한테 보여줘야함)
     UseAxios.post('/chats/enter', null, {
       params: {
-        boardId: socket.boardId,
-        vote: socket.userVoteData.userVote,
+        boardId: boardData.boardId,
+        vote: boardData.userVote,
       },
     })
       .then((res) => {
+        socket.join(boardData.boardId); // 방 들어감
+
+        const userVoteData = {
+          userId: userData.userId,
+          nickname: userData.nickname,
+          img: userData.img,
+          userVote: boardData.userVote,
+        };
+
+        socket.userVoteData = userVoteData;
+        socket.boardId = boardData.boardId;
+
+        if (userList.get(boardData.boardId) == undefined)
+          userList.set(boardData.boardId, new Set());
+
+        userList.get(boardData.boardId).add(socket); // back에서 가지고 있을 userList (나중에 새로 들어온 사용자한테 보여줘야함)
         // io.sockets.clients(boardData.boardId);
         let userDataList = Array.from(
           userList.get(boardData.boardId),
           (socket) => socket.userVoteData,
         );
 
-        socket.emit('user_enter', userDataList, userVoteData); // 본인한테만 전달
+        socket.emit('user_enter', userDataList); // 본인한테만 전달
         socket
           .to(boardData.boardId)
-          .emit('welcome', userVoteData, userDataList.length); // 본인 외 다른 참가자한테 전달
+          .emit('welcome', userVoteData); // 본인 외 다른 참가자한테 전달
       })
       .catch((err) => {
         console.log('enter failed');
@@ -67,6 +67,7 @@ io.on('connection', (socket) => {
     const userVoteData = socket.userVoteData;
     socket.to(socket.boardId).emit('send', userVoteData, msg);
   });
+
   // socket.on('betray', (boardId, userVoteData, opt1Cnt, opt2Cnt) => {
   socket.on('betray', (opt1Cnt, opt2Cnt) => {
     // 해당 user의 vote 변경
@@ -81,62 +82,43 @@ io.on('connection', (socket) => {
     io.to(boardId).emit('betray', user, opt1Cnt, opt2Cnt);
   });
 
-  // // 유저 입장
-  // socket.on("join", (data) => {
-  //   userList.push(data);
-  //   console.log("join" + data);
-  //   socket.name = data;
-  //   // 유저 정보 갱신
-  //   io.emit("updateUser", userList);
-  //   // 유저 입장 알리기(입장은 발신자 제외)
-  //   socket.broadcast.emit("joinUser", data);
-  // });
-  //
-  // // 메시지 보내기
-  // socket.on("msg", (data) => {
-  //   console.log("message: " + data);
-  //   io.emit("sm", {
-  //     name: socket.name,
-  //     msg: data,
-  //   }); // 발신자를 포함해서 모두 보내는것
-  //   // socket.broadcast.emit("sm", data); //발신자 제외
-  // });
-
   socket.on('disconnecting', () => {
-    userList.get(socket.boardId).delete(socket);
-    if (userList.get(socket.boardId).size == 0) {
-      UseAxios.post('/chats/delete', null, {
-        params: {
-          boardId: socket.boardId,
-        },
-      })
-        .then((res) => {
-          console.log('Delete Room Success');
-          userList.delete(socket.boardId);
+    if (socket.boardId) {
+      userList.get(socket.boardId).delete(socket);
+      if (userList.get(socket.boardId).size == 0) {
+        UseAxios.post('/chats/delete', null, {
+          params: {
+            boardId: socket.boardId,
+          },
         })
-        .catch((err) => {
-          console.log('Delete Room Failed');
-        });
-    } else {
-      UseAxios.post('/chats/leave', null, {
-        params: {
-          boardId: socket.boardId,
-          vote: socket.userVoteData.userVote,
-        },
-      })
-        .then((res) => {
-          console.log('Leave Room Success');
-          socket
-            .to(socket.boardId)
-            .emit(
-              'left',
-              socket.userVoteData,
-              userList.get(socket.boardId).size,
-            );
+            .then((res) => {
+              console.log('Delete Room Success');
+              userList.delete(socket.boardId);
+            })
+            .catch((err) => {
+              console.log('Delete Room Failed');
+            });
+      } else {
+        UseAxios.post('/chats/leave', null, {
+          params: {
+            boardId: socket.boardId,
+            vote: socket.userVoteData.userVote,
+          },
         })
-        .catch((err) => {
-          console.log('Leave Room Failed');
-        });
+            .then((res) => {
+              console.log('Leave Room Success');
+              socket
+                  .to(socket.boardId)
+                  .emit(
+                      'left',
+                      socket.userVoteData,
+                      userList.get(socket.boardId).size,
+                  );
+            })
+            .catch((err) => {
+              console.log('Leave Room Failed');
+            });
+      }
     }
   });
 });
