3857133c4075348fde16c9e0dba5795f2325e788
이보나 <bona17@naver.com>
2022-08-10 15:47:41 +0900

    FE_220810_design: 디폴트 프로필 사진 완료

--- frontend/src/navigations/HomeStack.js
+++ frontend/src/navigations/HomeStack.js
@@ -6,7 +6,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import LoginNav from './LoginNav';
 import SearchNav from './SearchNav';
 import UserContext from '../util/UserContext';
-import YourPageScreen from '../screens/YourPageScreen'
+import YourPageScreen from '../screens/YourPageScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialTopTabNavigator();
@@ -15,22 +15,58 @@ function HomeStack() {
   const {userData, setUserData} = useContext(UserContext);
   return (
     <Stack.Navigator>
-      {userData === null ?// 로그인 X
-      (<>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}   options={{headerShown: false}} />
-        <Stack.Screen name="LoginNav"   component={LoginNav}     options={{title: '로그인', headerShown: false,}}/>
-        <Stack.Screen name="SearchNav"  component={LoginNav}    options={{title: '검색', headerShown: false,}}/>
-        <Stack.Screen name="DetailScreen" component={DetailScreen} options={{title: '디테일',}}/>
-        <Stack.Screen name="YourPageScreen" component={YourPageScreen} options={{title: '유저페이지',}}/>
-      </>) : 
-      
-      (<>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}   options={{headerShown: false}} />
-        <Stack.Screen name="SearchNav"  component={SearchNav}    options={{title: '검색', headerShown: false}}/>
-        <Stack.Screen name="DetailScreen" component={DetailScreen} options={{title: '디테일',}}/>
-        <Stack.Screen name="YourPageScreen" component={YourPageScreen} options={{title: '유저페이지', headerShown: false,}}/>
-      </>)}
-      
+      {userData === null ? ( // 로그인 X
+        <>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="LoginNav"
+            component={LoginNav}
+            options={{title: '로그인', headerShown: false}}
+          />
+          <Stack.Screen
+            name="SearchNav"
+            component={LoginNav}
+            options={{title: '검색', headerShown: false}}
+          />
+          <Stack.Screen
+            name="DetailScreen"
+            component={DetailScreen}
+            options={{title: '디테일'}}
+          />
+          <Stack.Screen
+            name="YourPageScreen"
+            component={YourPageScreen}
+            options={{title: '유저페이지'}}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SearchNav"
+            component={SearchNav}
+            options={{title: '검색', headerShown: false}}
+          />
+          <Stack.Screen
+            name="DetailScreen"
+            component={DetailScreen}
+            options={{title: '디테일'}}
+          />
+          <Stack.Screen
+            name="YourPageScreen"
+            component={YourPageScreen}
+            options={{title: '유저페이지', headerShown: false}}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
