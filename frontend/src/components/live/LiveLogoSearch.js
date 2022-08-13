108f745093f86bebffedeb0d8cd8831f9eaa5516
김민균 <mingyun0324@gmail.com>
2022-08-13 22:39:57 +0900

    FE_220813_minimum : 회원탈퇴 디자인 통일, 프로필 기본사진 변경, 라이브 헤더 크기 같게 완료

--- frontend/src/components/live/LiveLogoSearch.js
+++ frontend/src/components/live/LiveLogoSearch.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     height: '7%',
-    padding: '3%',
+    paddingHorizontal: 16,
   },
   logo: {
     width: 70,
