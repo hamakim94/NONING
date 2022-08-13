527b00ab35face87c0800ce4d6ebba0a477d8318
이승연 <seongyeon38@gmail.com>
2022-08-13 22:40:14 +0900

    FE_220813_ssl : node server ssl 적용 test 진행중

--- frontend/src/screens/live/ChatScreen.js
+++ frontend/src/screens/live/ChatScreen.js
@@ -42,7 +42,7 @@ export default function ChatScreen({route, navigation}) {
   useEffect(() => {
     if (isFocused) {
       // socket = io(`http://10.0.2.2:3000`, {
-      socket = io(`https://i7a202.p.ssafy.io/`, {
+      socket = io(`https://i7a202.p.ssafy.io`, {
         transports: ['websocket'], // you need to explicitly tell it to use websockets
       });
 
