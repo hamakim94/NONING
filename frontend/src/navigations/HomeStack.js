47beaf482007e30f9a6fd1f1e75a160cec9c5e08
김민균 <mingyun0324@gmail.com>
2022-08-14 20:14:53 +0900

    FE_220814_header : 모든 헤더 통일 완료

--- frontend/src/navigations/HomeStack.js
+++ frontend/src/navigations/HomeStack.js
@@ -10,6 +10,13 @@ import YourPageScreen from '../screens/YourPageScreen';
 import FollowerScreen from '../screens/FollowerScreen';
 import FollowingScreen from '../screens/FollowingScreen';
 import Feather from 'react-native-vector-icons/Feather';
+import SettingScreen from '../screens/userEdit/SettingScreen';
+import ProfileEditScreen from '../screens/userEdit/ProfileEditScreen';
+import PasswordEditScreen from '../screens/userEdit/PasswordEditScreen';
+import InquiryScreen from '../screens/userEdit/InquiryScreen';
+import TosScreen from '../screens/userEdit/TosScreen';
+import PrivacyScreen from '../screens/userEdit/PrivacyScreen';
+import UserWithdrawalScreen from '../screens/userEdit/UserWithdrawalScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialTopTabNavigator();
@@ -83,6 +90,48 @@ function HomeStack() {
             name="FollowingScreen"
             component={FollowingScreen}
             options={HeaderOptions('Following')}
+          />
+          <Stack.Screen
+            name="SettingScreen"
+            component={SettingScreen}
+            options={HeaderOptions('설정')}
+          />
+          <Stack.Screen
+            name="ProfileEditScreen"
+            component={ProfileEditScreen}
+            options={HeaderOptions('프로필편집')}
+          />
+          <Stack.Screen
+            name="PasswordEditScreen"
+            component={PasswordEditScreen}
+            options={HeaderOptions('비밀번호변경')}
+          />
+          <Stack.Screen
+            name="InquiryScreen"
+            component={InquiryScreen}
+            options={HeaderOptions('문의하기')}
+          />
+          <Stack.Screen
+            name="TosScreen"
+            component={TosScreen}
+            options={HeaderOptions('서비스이용약관')}
+          />
+          <Stack.Screen
+            name="PrivacyScreen"
+            component={PrivacyScreen}
+            options={HeaderOptions('개인정보')}
+          />
+          <Stack.Screen
+            name="UserWithdrawalScreen"
+            component={UserWithdrawalScreen}
+            options={{
+              title: '회원 탈퇴',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+              headerTitleAlign: 'center',
+              headerShadowVisible: false,
+            }}
           />
         </>
       )}
--- frontend/src/navigations/LiveNav.js
+++ frontend/src/navigations/LiveNav.js
@@ -6,6 +6,13 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import YourPageScreen from '../screens/YourPageScreen';
 import FollowerScreen from '../screens/FollowerScreen';
 import FollowingScreen from '../screens/FollowingScreen';
+import SettingScreen from '../screens/userEdit/SettingScreen';
+import ProfileEditScreen from '../screens/userEdit/ProfileEditScreen';
+import PasswordEditScreen from '../screens/userEdit/PasswordEditScreen';
+import InquiryScreen from '../screens/userEdit/InquiryScreen';
+import TosScreen from '../screens/userEdit/TosScreen';
+import PrivacyScreen from '../screens/userEdit/PrivacyScreen';
+import UserWithdrawalScreen from '../screens/userEdit/UserWithdrawalScreen';
 const Stack = createNativeStackNavigator();
 export default function LiveNav() {
   return (
@@ -34,6 +41,48 @@ export default function LiveNav() {
         name="FollowingScreen"
         component={FollowingScreen}
         options={HeaderOptions('Following')}
+      />
+      <Stack.Screen
+        name="SettingScreen"
+        component={SettingScreen}
+        options={HeaderOptions('설정')}
+      />
+      <Stack.Screen
+        name="ProfileEditScreen"
+        component={ProfileEditScreen}
+        options={HeaderOptions('프로필편집')}
+      />
+      <Stack.Screen
+        name="PasswordEditScreen"
+        component={PasswordEditScreen}
+        options={HeaderOptions('비밀번호변경')}
+      />
+      <Stack.Screen
+        name="InquiryScreen"
+        component={InquiryScreen}
+        options={HeaderOptions('문의하기')}
+      />
+      <Stack.Screen
+        name="TosScreen"
+        component={TosScreen}
+        options={HeaderOptions('서비스이용약관')}
+      />
+      <Stack.Screen
+        name="PrivacyScreen"
+        component={PrivacyScreen}
+        options={HeaderOptions('개인정보')}
+      />
+      <Stack.Screen
+        name="UserWithdrawalScreen"
+        component={UserWithdrawalScreen}
+        options={{
+          title: '회원 탈퇴',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+        }}
       />
     </Stack.Navigator>
   );
