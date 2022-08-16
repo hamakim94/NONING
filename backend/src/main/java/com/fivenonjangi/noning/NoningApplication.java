e935d89ee8fada757b9f195e8a93b42f2d097eb7
이재순 <dlwotns1112@gmail.com>
2022-08-16 22:07:35 +0900

    BE_220816_Timezone:한국시간 설정 완료

--- backend/src/main/java/com/fivenonjangi/noning/NoningApplication.java
+++ backend/src/main/java/com/fivenonjangi/noning/NoningApplication.java
@@ -4,8 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class NoningApplication {
+
+    @PostConstruct
+    public void started() {
+        // timezone UTC 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NoningApplication.class, args);
