c9a6f194ddd55a6f794df8bbb14fa6ed00bed247
이재순 <dlwotns1112@gmail.com>
2022-08-16 12:44:58 +0900

    BE_220816_CHAT : port 수정

--- chatserver/src/config.js
+++ chatserver/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   listenIp: '0.0.0.0',
-  listenPort: 3001,
+  listenPort: 3000,
   sslCrt: '/etc/ssl/certs/ssl-cert-snakeoil.pem',
   sslKey: '/etc/ssl/private/ssl-cert-snakeoil.key',
   mediasoup: {
